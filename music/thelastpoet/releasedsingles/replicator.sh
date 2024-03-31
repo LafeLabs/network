496cd37d616153c3f30f336d648580b1b3f14798
Trash Robot <lafelabs@gmail.com>
2024-03-30 21:19:30 -0600

    put singles album in proper configuration

---
+++ music/thelastpoet/releasedsingles/replicator.sh
@@ -0,0 +1,15 @@
+mkdir releasedsingles
+cd releasedsingles
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/album/replicator.php
+php replicator.php
+cd tracks
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/tracks/The%20Last%20Poet%20-%20Don%27t%20Play%20Wit%27%20It%20Hip%20Hop%20Baby%20Mix.mp3
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/tracks/The%20Last%20Poet%20-%20Lyrical.mp3
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/tracks/The%20Last%20Poet%20-%20New%20York%20Minute.mp3
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/tracks/The%20Last%20Poet%20-%20No%20Features%20%28beat%20by%20Admiral%20Atlas%29.mp3
+cd ../images
+cd ..
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/index.html
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/replicator.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/README.md
+cd ..
