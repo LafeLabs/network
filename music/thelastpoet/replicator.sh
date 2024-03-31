a9d154dc5fbc29b6b6c0b5a9764617e19bb76433
Trash Robot <lafelabs@gmail.com>
2024-03-30 22:16:24 -0600

    Update replicator.sh

--- music/thelastpoet/replicator.sh
+++ music/thelastpoet/replicator.sh
@@ -1,15 +1,15 @@
 mkdir thelastpoet
 cd thelastpoet
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/apagefrommybook/replicator.sh
-sh replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/fuckrap/replicator.sh
-sh replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/hiphopbaby/replicator.sh
-sh replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/replicator.sh
-sh replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/thelastpoem/replicator.sh
-sh replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/unfinishedunreleased/replicator.sh
-sh replicator.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/apagefrommybook/replicator.sh -O apagefrommybook.sh
+sh apagefrommybook.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/fuckrap/replicator.sh -O fuckrap.sh
+sh fuckrap.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/hiphopbaby/replicator.sh -O hiphopbaby.sh
+sh hiphopbaby.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/releasedsingles/replicator.sh -O releasedsingles.sh
+sh releasedsingles.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/thelastpoem/replicator.sh -O thelastpoem.sh
+sh thelastpoem.sh
+wget https://raw.githubusercontent.com/LafeLabs/network/main/music/thelastpoet/unfinishedunreleased/replicator.sh -O unfinishedunreleased.sh
+sh unfinishedunreleased.sh
 cd ..
