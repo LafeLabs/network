c7e775799fe64367e8f40f615d595653dc5267f7
Trash Robot <lafelabs@gmail.com>
2024-04-24 22:07:55 -0600

    gutenberg library clone

--- books/gutenberg/replicator.sh
+++ books/gutenberg/replicator.sh
@@ -1,13 +1,41 @@
-mkdir mynewset
-cd mynewset
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/replicator.php
+mkdir gutenberg
+cd gutenberg
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.php
 php replicator.php
-cd elements
-cd ../images
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/images/qrcode-page.png
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/images/qrcode.png
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/scrolls/home -O scrolls/home
+
+mkdir aliceinwonderland
+cd aliceinwonderland
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.php
+php replicator.php
+wget https://raw.githubusercontent.com/LafeLabs/pibrary/main/gutenberg/aliceinwonderland/data/scrollset.txt -O data/scrollset.txt
+php scrollsetreplicator.php
 cd ..
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/index.html -O index.html
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/replicator.sh -O replicator.sh
-wget https://raw.githubusercontent.com/LafeLabs/network/main/page/README.md -O README.md
+
+mkdir lookingglass
+cd lookingglass
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.php
+php replicator.php
+wget https://raw.githubusercontent.com/LafeLabs/pi/main/library/lookingglass/data/scrollset.txt -O data/scrollset.txt
+php scrollsetreplicator.php
 cd ..
+
+mkdir wizard-of-oz
+cd wizard-of-oz
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.php
+php replicator.php
+wget https://raw.githubusercontent.com/LafeLabs/trashbook/main/gutenberg/fiction/wizard-of-oz/data/scrollset.txt -O data/scrollset.txt
+php scrollsetreplicator.php
+cd ..
+
+mkdir KJV
+cd KJV
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.php
+php replicator.php
+wget https://raw.githubusercontent.com/LafeLabs/pibrary/main/gutenberg/chaosbible/data/scrollset.txt -O data/scrollset.txt
+php scrollsetreplicator.php
+cd ..
+
+
+wget https://raw.githubusercontent.com/LafeLabs/network/main/books/gutenberg/replicator.sh -O replicator.sh
+
